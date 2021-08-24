df717fd886ebde31581ef58c5c9193188c1acad1
Jakson Casas <jaksoncasas@gmail.com>
2021-08-24 16:06:57 -0500

    Actualizar la vista y configurar las urls

--- TauraikApp/views.py
+++ TauraikApp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, HttpResponse
 # Create your views here.
 def home(request):
-    return HttpResponse("Inicio")
+    return HttpResponse("Home")
 
 def servicios(request):
     return HttpResponse("Servicios")
