c3e699761ebe035d3e6cce8d56c7734e6f71266e
Jakson Casas <jaksoncasas@gmail.com>
2021-08-26 09:46:27 -0500

    Agregar clase Servicio con sus caracteristicas

--- servicios/models.py
+++ servicios/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Servicio(models.Model):
+    titulo = models.CharField(max_length=50)
+    contenido = models.CharField(max_length=50)
+    imagen = models.ImageField()
+    created = models.CharField(max_length=50)
+    updated = models.CharField(max_length=50)
