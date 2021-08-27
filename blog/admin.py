d2cf302bd2ce3c8c042a022dde2f4ab9b78e907e
Jakson Casas <jaksoncasas@gmail.com>
2021-08-27 10:29:19 -0500

    Agregar las clases creada hacia el panel de administración

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
-
+from django.db import models
+#Importar el modelo categoria y post
+from .models import Categoria,Post
 # Register your models here.
+
+#Agregar las clases creada hacia el panel de administración
+
+class CategoriaAdmin(admin.ModelAdmin):
+    readonly_fields=('created','updated')
+
+class PostAdmin(admin.ModelAdmin):
+    readonly_fields=('created','updated')
+    
+
+admin.site.register(Categoria, CategoriaAdmin)
+admin.site.register(Post, PostAdmin)
