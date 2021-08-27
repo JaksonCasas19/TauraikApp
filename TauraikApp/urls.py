13e70b6c0411a8d72d04bba1b3670e7ede4ced26
Jakson Casas <jaksoncasas@gmail.com>
2021-08-26 23:07:44 -0500

    Agregar y configurar directorio Media para alojar imagenes

--- TauraikApp/urls.py
+++ TauraikApp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from TauraikApp import views
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name="Home"),
@@ -9,3 +10,6 @@ urlpatterns = [
     path('blog/', views.blog, name="Blog"),
     path('contacto/', views.contacto, name="Contacto"),
 ]
+
+#Agregar el directorio Media para ver la imagen desde el panel de admin
+urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
