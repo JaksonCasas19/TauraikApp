5129a2b58b6734f3b219f030f7e82382373d685c
Jakson Casas <jaksoncasas@gmail.com>
2021-08-28 19:00:43 -0500

    Configurar categoria

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.blog, name="Blog"),
+    path('categoria/<int:categoria_id>/', views.categoria, name="categoria")
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
-from blog.models import Post 
+from blog.models import Categoria, Post 
 
 # Create your views here.
 def blog(request):
     posts = Post.objects.all() #Importar todos los objectos guardo en Post
     return render(request, "blog/blog.html", {"posts":posts})
+
+
+def categoria(request,categoria_id):
+    categoriaOk=Categoria.objects.get(id=categoria_id) #Se usa como un where para traer solo esa categoria
+    post=Post.objects.filter(categoria=categoriaOk)
+    return render(request, "blog/categoria.html",{'categoria':categoria,'post':post})
