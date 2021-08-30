79596788e4a32d9e0796bf1cc245f1c2fc041315
Jakson Casas <jaksoncasas@gmail.com>
2021-08-30 16:07:41 -0500

    Agregar forms en la plantilla Contacto y darle formato en la hoja de estilo

--- contacto/forms.py
+++ contacto/forms.py
@@ -3,4 +3,4 @@ from django import forms
 class FormularioContacto(forms.Form):
     nombre = forms.CharField(label="Nombre",required=True)
     email = forms.CharField(label="Email",required=True)
-    contenido = forms.CharField(label="Contenido")
+    contenido = forms.CharField(label="Contenido",widget=forms.Textarea) #Para agrandar el cuadro (widget=forms.Textarea)
