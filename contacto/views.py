dbb717a330f94587fb27a2162a422b1be3a3bebf
Jakson Casas <jaksoncasas@gmail.com>
2021-08-30 11:04:45 -0500

    Agregar formulario a views y luego aplicar el formulario a template

--- contacto/views.py
+++ contacto/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
+from .forms import FormularioContacto
+
 # Create your views here.
 def contacto(request):
-    return render(request, "contacto/contacto.html")
+    formulario_contacto=FormularioContacto() #Instanciar
+    return render(request, "contacto/contacto.html",{"miFormulario":formulario_contacto})
