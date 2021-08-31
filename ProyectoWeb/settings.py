5770094bb9246d6b41a7358ad21733c93b4ab257
Jakson Casas <jaksoncasas@gmail.com>
2021-08-31 11:16:47 -0500

    Configurar servicio para envio de correo en settings y en views.py contacto

--- ProyectoWeb/settings.py
+++ ProyectoWeb/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+!#ush)#qv6qj-)w3n4rvgrep^7!+(n$fb0rb$45iz$$b+bwct'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG')
 
 ALLOWED_HOSTS = []
 
@@ -130,3 +131,12 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+#DataFlair
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = config('USER_EMAIL')
+EMAIL_HOST_PASSWORD = config('USER_MAIL_PASSWORD')
--- contacto/views.py
+++ contacto/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import redirect, render
-
 from .forms import FormularioContacto
+
+from django.core.mail import EmailMessage
+from decouple import config
 
 # Create your views here.
 def contacto(request):
@@ -13,7 +15,15 @@ def contacto(request):
             email=request.POST.get("email")
             contenido=request.POST.get("contenido")
 
-            #Redireccionar un parametro o un mensaje
-            return redirect("/contacto/?valido")
+            email = EmailMessage("Mensaje desde App Django",
+            "El usuario con nombre {} con la direccion {} escribe lo siguiente:\n\n{}".format(nombre,email,contenido),"",[config('USER_EMAIL')],reply_to=[email])
+
+            try:
+                #Envia el email que se declaro en la linea 18
+                email.send()
+                #Redireccionar un parametro o un mensaje
+                return redirect("/contacto/?valido")
+            except:
+                return redirect("/contacto/?novalido")
 
     return render(request, "contacto/contacto.html",{"miFormulario":formulario_contacto})
