84183e09a4e56901f1f2fb6070944a52e0a3620f
Jakson Casas <jaksoncasas@gmail.com>
2021-09-01 17:09:05 -0500

    Crear clase Carro y funcion Agregar

---
+++ carro/carro.py
@@ -0,0 +1,27 @@
+class Carro:
+    def __init__(self,request):
+        #Cuando el usuario agrege por primera vez un Item, esta linea se encargara de guardar los elementos en el carro
+        #Si el usuario se va y vuelve debe mantener dicha información
+        self.request=request
+        self.session=request.session 
+
+        carro = self.session.get("carro")
+
+        if not carro:
+            #Si no hay carro  debemos retornar vacio
+            carro = self.session["carro"]={}
+        else:
+            #Si el usuario agrego al carro
+            self.carro=carro
+
+    def agregar(self,producto):
+
+        #Si el Id del producto no esta en las claves de nuestro carro me lo agregas
+        if(str(producto.id)not in self.carro.keys()):
+            self.carro[producto.id] = {
+                "producto_id":producto.id,
+                "nombre":producto.nombre,
+                "precio":str(producto.precio),
+                "cantidad":1,
+                "imagen":producto.imagen,
+            }
