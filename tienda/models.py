7dc07ea6e5b8ad8d1c2b8dde94f1bb6e088ccd6a
Jakson Casas <jaksoncasas@gmail.com>
2021-09-01 11:09:41 -0500

    Llamar el modelo producto a vista

--- tienda/models.py
+++ tienda/models.py
@@ -17,13 +17,15 @@ class CategoriaProd(models.Model):
 class Producto(models.Model):
     nombre = models.CharField(max_length=50)
     categorias = models.ForeignKey(CategoriaProd, on_delete=models.CASCADE) #Clave foranea - Referencia
-    imagen = models.ImageField(upload_to="tienda", null=True, blank=True)
+    imagen = models.CharField(max_length=100,null=True, blank=True)
     precio = models.FloatField()
     disponibilidad = models.BooleanField(default=True)
-    
+
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         verbose_name = "Producto"
-        verbose_name_plural = "Productos"   
+        verbose_name_plural = "Productos" 
+    def __str__(self):
+        return self.nombre
--- tienda/views.py
+++ tienda/views.py
@@ -1,5 +1,8 @@
+from tienda.models import Producto
 from django.shortcuts import render
+from .models import Producto
 
 def tienda(request):
-   return render(request, "tienda/tienda.html")
+    productos = Producto.objects.all()
+    return render(request, "tienda/tienda.html",{"productos":productos})
   
