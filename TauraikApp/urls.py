3a093d5d4fedad9f939f69b560432edd1cb1f177
Jakson Casas <jaksoncasas@gmail.com>
2021-09-01 09:59:38 -0500

    Configurar urls,views y setting tienda

--- TauraikApp/urls.py
+++ TauraikApp/urls.py
@@ -5,7 +5,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name="Home"),
-    path('tienda/', views.tienda, name="Tienda"),
 ]
 
 #Agregar el directorio Media para ver la imagen desde el panel de admin
