23f05258c511d6eb67e9517ea84cdf14c9e9c77d
Jakson Casas <jaksoncasas@gmail.com>
2021-09-02 22:48:15 -0500

    Test context_processor

--- carro/context_processor.py
+++ carro/context_processor.py
@@ -1,6 +1,6 @@
 
 def importe_total_carro(request):
-    total = 0
+    total = 10
     if request.user.is_authenticated:
         for key, value in request.session["carro"].items():
             total = total+(float(value["precio"])*value["cantidad"])
