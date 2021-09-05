a03df4d501c734e8fcf8ecc73ef1f8e6fa358d7a
Jakson Casas <jaksoncasas@gmail.com>
2021-09-05 10:19:51 -0500

    Agregar total en context-processor

--- carro/context_processor.py
+++ carro/context_processor.py
@@ -1,7 +1,14 @@
 
 def importe_total_carro(request):
     total = 0
-    #if request.user.is_authenticated: Usar para utenticar
+    totalAll= 0
+
+    if request.user.is_authenticated:
+        for key, value in request.session["carro"].items():
+            total = total+(float(value["precio"])*value["cantidad"])
+    
     for key, value in request.session["carro"].items():
-        total = total+(float(value["precio"])*value["cantidad"])
-    return {"importe_total_carro":total}
+        total = total+(float(value["precio"]))
+        totalAll = totalAll + value["cantidad"]
+
+    return {"importe_total_carro":total,"cantidad_general":totalAll}
