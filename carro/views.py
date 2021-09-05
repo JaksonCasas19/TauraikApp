9333294e2eb767cc9e5be34168c3a17b63b7cdf4
Jakson Casas <jaksoncasas@gmail.com>
2021-09-04 21:39:50 -0500

    Modificar views

--- carro/views.py
+++ carro/views.py
@@ -3,7 +3,6 @@ from django.shortcuts import render
 from .carro import Carro
 from tienda.models import Producto
 from django.shortcuts import redirect
-# Create your views here.
 
 def agregar_producto(request, producto_id):
     carro=Carro(request)
