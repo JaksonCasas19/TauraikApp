1fcf98cfba429fbdedf35f0fe3e0b206c41952a5
Jakson Casas <jaksoncasas@gmail.com>
2021-09-06 17:07:50 -0500

    Test 2 sqlite3

--- carro/context_processor.py
+++ carro/context_processor.py
@@ -3,12 +3,13 @@ def importe_total_carro(request):
     total = 0
     totalAll= 0
 
-    if request.user.is_authenticated:
+    if request.user:
         for key, value in request.session["carro"].items():
             total = total+(float(value["precio"])*value["cantidad"])
+            totalAll = totalAll + value["cantidad"]
     
-    for key, value in request.session["carro"].items():
-        total = total+(float(value["precio"]))
-        totalAll = totalAll + value["cantidad"]
-
+    #for key, value in request.session["carro"].items():
+     #   total = total+(float(value["precio"]))
+     #   totalAll = totalAll + value["cantidad"]
+#"importe_total_carro":total,"cantidad_general":totalAll
     return {"importe_total_carro":total,"cantidad_general":totalAll}
