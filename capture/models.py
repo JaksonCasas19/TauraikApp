97fe9c29c67c4c50373a4a70d7e8da2d59196876
Jakson Casas <jaksoncasas@gmail.com>
2021-09-09 15:59:15 -0500

    Agregar Ajax capture e implementar en views y models

--- capture/models.py
+++ capture/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class captureUser(models.Model):
+    name = models.CharField(max_length=32)
+    texto = models.CharField(max_length=32)
+    qr = models.CharField(max_length=50)
--- capture/views.py
+++ capture/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 def capture(request):
     #miListas = Check.objects.all()
     return render(request, "capture/capture.html")
+
+def create_user(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        texto = request.POST['texto']
+        qr = request.POST['qr']
